fe31e72efd7546d0e2ef68bc1b178147645a213a
SanjidaOSD <sanjidaema161443@gmail.com>
2024-02-19 11:26:08 +0600

    added total price section

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,45 @@
+let seatNumber = 0;
+let sub = 8;
 
-// function handleClick(e){
-//     const selectTicket = document.getElementById('ticket-sale');
-//     console.log(selectTicket);
-    
-// }
+
 const allBtn = document.getElementsByClassName('btn');
 
 for(let btn of allBtn){
+  
     btn.addEventListener('click', function(){
         btn.style.backgroundColor = 'green';
-    })
-}
-
-
-
-
-let count = 0;
-
-for(const btn of allBtn){
-    btn.addEventListener('click', function(e){
-        count = count + 1;
-         setInnerText('ticket-count',count);  
-        })
-        
     
-}
+        if(seatNumber === 4){
+            return alert ("You can't added other seat")
+        }
+            seatNumber = seatNumber + 1;
+             setInnerText('ticket-count',seatNumber);
+             
+             sub = sub - 1;
+             setInnerText('seat-count', sub);
 
-let sub = 8;
+             
+const totalTicketPrice = document.getElementById('total-price')
+const totalPrice = 550 * totalTicketPrice;
+console.log(totalPrice)
+            //  const totalSelected = document.getElementById('total-selected')
+            //  const newSelected = document.createElement('p')
+            //  newElement.innerText = `seat price is : ${550}`
+            //  totalSelected.appendChild(newSelected)
+    
+
+              
+            })
+          
+           
+}
+       
+
+
+
+
+
+
 
 
 
